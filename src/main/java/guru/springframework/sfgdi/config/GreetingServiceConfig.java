80de1ebcd8a34bd1cffd95e8b36748379de2cc2d
ozlemcetin <ozlem.cetin@gmail.com>
2022-08-25 22:21:21 +0300

    Spring XML Configuration Example

--- src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
+++ src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
@@ -4,11 +4,9 @@ package guru.springframework.sfgdi.config;
 import guru.springframework.sfgdi.repositories.EnglishGreetingRepository;
 import guru.springframework.sfgdi.repositories.GreetingRepository;
 import guru.springframework.sfgdi.services.*;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.*;
 
+@ImportResource("classpath:sfgdi-config.xml")
 @Configuration
 public class GreetingServiceConfig {
 
@@ -33,10 +31,12 @@ public class GreetingServiceConfig {
         return new GreetingServiceESImpl();
     }
 
-    @Bean
-    GreetingServiceFRImpl greetingServiceFRImpl() {
-        return new GreetingServiceFRImpl();
-    }
+    /*
+        @Bean
+        GreetingServiceFRImpl greetingServiceFRImpl() {
+            return new GreetingServiceFRImpl();
+        }
+     */
 
 
     /*
